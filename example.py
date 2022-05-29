9a038d206bf02b32693a7bdd7f5626bdde8ef998
Merkz <91028537+ItsMerkz@users.noreply.github.com>
2022-05-29 20:55:41 +0100

    Update example.py

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ import requests
 import os 
 
 hwid = str(str(subprocess.check_output('wmic csproduct get uuid')).strip().replace(r"\r", "").split(r"\n")[1].strip())
-resp = httpx.get("") # put your link that is displayed once you run the server.py here, make sure its open port if your planning to use it!
+resp = httpx.get("", data={"hwid": hwid}) # put your link that is displayed once you run the server.py here, make sure its open port if your planning to use it!
 if resp.text == "Authed":
     print("Your Authed, Congrats...")
     # code here, maybe call the function 
