b5adda1e486c082dde47133ffe338cfb2cae4e63
Merkz <91028537+ItsMerkz@users.noreply.github.com>
2023-01-13 21:07:41 +0000

    Update example.py

--- example.py
+++ example.py
@@ -2,14 +2,17 @@ import httpx
 import subprocess
 import requests 
 import os 
+import json 
+
+config = json.load(open("config.json", encoding="utf-8", errors="ignore"))
 
 hwid = str(str(subprocess.check_output('wmic csproduct get uuid')).strip().replace(r"\r", "").split(r"\n")[1].strip())
-resp = httpx.get("", data={"hwid": hwid}) # put your link that is displayed once you run the server.py here, make sure its open port if your planning to use it!
+resp = requests.post("%s/hwids" % (config["Server"]["Url"]), data={"hwid": hwid}) 
 if resp.text == "Authed":
     print("Your Authed, Congrats...")
     # code here, maybe call the function 
-if resp.text == "Not Authed":
-    print("Your Not Authed!")
+elif resp.text == "Not Authed!":
+    print("Send Your Hwid To An Owner [%s]" % (hwid))
     os.system("exit")
 else:
     print("Error...")
